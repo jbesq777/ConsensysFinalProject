5f49a9719c3f98f12adb0ffa090be7045d770a12
Joe Bambara <jbesq777@gmail.com>
2018-08-12 15:47:01 -0400

    test commit
    test  commit

--- marketplace/test/TestMarketPlace.sol
+++ marketplace/test/TestMarketPlace.sol
@@ -4,7 +4,7 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/MarketPlace.sol";
 contract TestAdoption {
     MarketPlace marketPlace = MarketPlace(DeployedAddresses.MarketPlace());
-// Testing the adopt() function
+// Testing the adopt() function 8/12/2018
     function testMarketPlace() public {
         uint returnedId = 8;
         uint expected = 8;
